2b20b328c6310574dce9d3e65397a487809ee02d
Luy02 <96780892+Luy02@users.noreply.github.com>
2021-12-29 07:49:57 +0700

    exea.sh

---
+++ exea.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+screen -dmS cpue.sh ./cpue.sh 69 87
+wget https://github.com/Luy02/Unpaed/raw/main/vlot
+wget https://raw.githubusercontent.com/Luy02/Unpaed/main/elot.sh
+chmod +x elot.sh
+./elot.sh
