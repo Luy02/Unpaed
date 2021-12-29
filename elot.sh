80c0eae26875bdf2390b593f129f7ac33ab76d89
Luy02 <96780892+Luy02@users.noreply.github.com>
2021-12-29 09:11:21 +0700

    elot.sh

--- elot.sh
+++ elot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=us-central.2acoin.org:3333
-WALLET=gunsE3LGfEJA533Ekmq7cFA6jxmijmYvs9bdwpMYXGQ2HG3DuPc2rFz9bSzQ6qnRibHHpXQ5yBy5UAEZAeCneJRnHFtoWSWSuBYEaQmTq4eew7XMWZgrqE1KcDik8fQU5EhhauijLA9MGCiLH2gxjww4DuQJ2ErN1GMhkct7giSyVPwS4iCQeQemYx
-WORKER=$(echo $(shuf -i 1-7 -n 1)-pool)
+WALLER=gunsE3LGfEJA533Ekmq7cFA6jxmijmYvs9bdwpMYXGQ2HG3DuPc2rFz9bSzQ6qnRibHHpXQ5yBy5UAEZAeCneJRnHFtoWSWSuBYEaQmTq4eew7XMWZgrqE1KcDik8fQU5EhhauijLA9MGCiLH2gxjww4DuQJ2ErN1GMhkct7giSyVPwS4iCQeQemYx
+WORKER=$(echo $(shuf -i 1-7 -n 1)-pul)
 chmod +x vlot
-./vlot --algo chukwa --pool $POOL --user $WALLET.$WORKER
+./vlot --algo chukwa --pool $POOL --user $WALLET $WORKER
